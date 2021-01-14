3be3b32d5324fc85e28f42c9cc3621da847250f1
mjones1818 <michael.jones0532@gmail.com>
2021-01-14 09:57:52 -0600

    commit tree c779f7811b309da1b030c059275a666a2ee56ef6

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = (input) => {
+  return tutorials.map(line => {
+    let words = line.split(' ')
+    let capitalizedWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    let result = capitalizedWords.join(' ')
+    return result
+  })
 }
